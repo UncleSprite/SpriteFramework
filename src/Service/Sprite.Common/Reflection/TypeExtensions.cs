ab704cc378578e72ab42d62116871841899125c1
UncleSprite <378819972@qq.com>
2020-03-18 08:19:07 +0800

    other"

--- src/Service/Sprite.Common/Reflection/TypeExtensions.cs
+++ src/Service/Sprite.Common/Reflection/TypeExtensions.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Sprite.Common.Extensions;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 
@@ -21,6 +23,69 @@ namespace Sprite.Common.Reflection
             where T : Attribute
         {
             return memberInfo.IsDefined(typeof(T), inherit);
+        }
+
+        /// <summary>
+        /// 从类型成员获取指定Attribute特性
+        /// </summary>
+        /// <typeparam name="TAttribute">Attribute特性类型</typeparam>
+        /// <param name="memberInfo">类型类型成员</param>
+        /// <param name="inherit">是否从继承中查找</param>
+        /// <returns>存在返回第一个，不存在返回null<</returns>
+        public static TAttribute GetAttribute<TAttribute>(this MemberInfo memberInfo, bool inherit = true)
+            where TAttribute : Attribute
+        {
+            var attributes = memberInfo.GetCustomAttributes(typeof(TAttribute), inherit);
+            return attributes.FirstOrDefault() as TAttribute;
+        }
+
+        /// <summary>
+        /// 当前类型是否是指定基类的派生类
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="baseType"></param>
+        /// <returns></returns>
+        public static bool IsBaseOn(Type type, Type baseType)
+        {
+            if (baseType.IsGenericTypeDefinition)
+                return baseType.IsGenericTypeAssignableFrom(type);
+
+            return baseType.IsAssignableFrom(type);
+        }
+
+        /// <summary>
+        /// 指定当前泛型是否可由指定的类型填充
+        /// </summary>
+        /// <param name="genericType">泛型类型</param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static bool IsGenericTypeAssignableFrom(this Type genericType, Type type)
+        {
+            genericType.CheckNotNull("genericType");
+            type.CheckNotNull("type");
+
+            if (!genericType.IsGenericType)
+                throw new ArgumentException("该功能只支持泛型类型的调用，非泛型类型可使用 IsAssignableFrom 方法。");
+
+            List<Type> allOthers = new List<Type>();
+            if (genericType.IsInterface)
+                allOthers.AddRange(type.GetInterfaces());
+
+            foreach (var other in allOthers)
+            {
+                Type cur = other;
+                while (cur != null)
+                {
+                    if (cur.IsGenericType)
+                        cur = cur.GetGenericTypeDefinition();
+
+                    if (cur.IsSubclassOf(genericType) || cur == genericType)
+                        return true;
+
+                    cur = cur.BaseType;
+                }
+            }
+            return false;
         }
     }
 }
